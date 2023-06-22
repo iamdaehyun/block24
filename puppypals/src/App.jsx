feb6dea611893eb2072c01c67ccbd83f769a4030
iamdaehyun <126280104+iamdaehyun@users.noreply.github.com>
2023-06-22 15:56:18 -0700

    completed

--- puppypals/src/App.jsx
+++ puppypals/src/App.jsx
@@ -1,5 +1,16 @@
-import { useState } from "react";
-import { puppyList } from "./data";
+import { puppyList } from "./data.js";
 console.log(puppyList);
 
-const [puppies, setPuppies] = useState(puppyList);
+return (
+  <>
+    <div>
+      {" "}
+      {}
+      {puppies.map((puppy) => {
+        return <p>{puppy.name}</p>;
+      })}
+    </div>
+  </>
+);
+
+export default App;
